c0687f5752596b886ff5c6e2295a04f2da9f1f60
Venkateshwaran S <54279094+venkatesh1996@users.noreply.github.com>
2026-08-17 11:55:50 +0530

    Initial commit: solution + project + Docker setup

---
+++ sampledeploy/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace sampledeploy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
